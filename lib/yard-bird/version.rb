05d1a5079d41d3b4021858c51d0ee3431d5dc8aa
7rans <transfire@gmail.com>
2011-11-09 12:54:13 -0500

    Dynamic in lib metadata from .ruby. [minor]

--- lib/yard-bird/version.rb
+++ lib/yard-bird/version.rb
@@ -1,6 +1,17 @@
 module YARD
   module Bird
-    VERSION = "0.1.1"
+    #
+    def self.dotruby
+      @dotruby ||= (
+        require 'yaml'
+        file = File.dirname(__FILE__)+'/../../.ruby'
+        File.exist?(file) ? YAML.load_file(file) : {}
+      )
+    end
+    #
+    def self.const_missing(name)
+      dotruby[name.to_s.downcase] || super(name)
+    end
   end
 end
 
